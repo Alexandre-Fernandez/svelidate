ca3f1cccb45d2a2ff70e06a3c4325a707a3370e5
alexandre-fernandez <alexandre-fernandez@gmx.com>
2022-06-14 22:50:22 +0200

    added isLookahead

--- src/utilities/regex.ts
+++ src/utilities/regex.ts
@@ -1,1 +1,7 @@
-function isLookahead() {}
+export function isLookahead(pattern: string) {
+	if (pattern.length < 4) return false
+	if (pattern[pattern.length - 1] !== ")") return false
+	if (pattern[0] !== "(" || pattern[1] !== "?") return false
+	if (pattern[2] !== "=" && pattern[2] !== "!") return false
+	return true
+}
