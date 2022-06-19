0bba679a996e24f9bbb392fc5a6d5c36dbe1f68c
alexandre-fernandez <alexandre-fernandez@gmx.com>
2022-06-19 13:40:54 +0200

    exports

--- src/index.ts
+++ src/index.ts
@@ -1,2 +1,21 @@
-export { svelidate } from "./form/"
-export { UninitializedForm, SvelidateForm, SvelidateFormStore } from "./types"
+export { svelidate } from "./form"
+export { svelidateConfig } from "./form/config"
+export {
+	UninitializedForm,
+	SvelidateForm,
+	SvelidateFormStore,
+	SvelidateConfiguration,
+	ValidatorWrapper,
+	JsValidator,
+	JsValidatorPredicate,
+	HtmlValidator,
+	HtmlValidatorMapper,
+} from "./types"
+export { validateIf } from "./validation/factories/validatorCollection"
+export {
+	createValidatorWrapperFactory,
+	createStringValidatorWrapperFactory,
+	createNumberValidatorWrapperFactory,
+	createDateValidatorWrapperFactory,
+} from "./validation/factories/validatorCollectionFactory"
+export * from "./validation/validators"
