0bafa799c82653b6af7bd9b08c221675d0c9e397
alexandre-fernandez <alexandre-fernandez@gmx.com>
2022-06-22 14:01:15 +0200

    added js validation on main formats

--- src/validation/validators/filelist.ts
+++ src/validation/validators/filelist.ts
@@ -1,4 +1,5 @@
 import type { ByteUnit } from "../../types"
+import { isAudio, isImage, isVideo } from "../../utilities/file"
 import { toBytes } from "../../utilities/general"
 import { getMatchingHtmlValidator } from "../../utilities/input"
 import { createFileListValidatorWrapperFactory } from "../factories/validatorCollectionFactory"
@@ -7,6 +8,37 @@ import { createFileListValidatorWrapperFactory } from "../factories/validatorCol
 // size
 // number of files
 // filenames ?
+/*
+let File = {
+	name: "black.png",
+	lastModified: 1643617999316,
+	webkitRelativePath: "",
+	size: 144, // bytes
+	type: "image/png",
+}
+{
+	name: "rotated-phone.xcf",
+	lastModified: 1644198965043,
+	webkitRelativePath: "",
+	size: 678609,
+	type: "image/x-xcf",
+}
+{
+	name: "rabbit.mp4",
+	lastModified: 1468153484000,
+	webkitRelativePath: "",
+	size: 346861418,
+	type: "video/mp4",
+}
+
+image/*
+".tiff", ".pjp", ".jfif", ".bmp", ".gif", ".svg", ".png", ".xbm", ".dib", ".jxl", ".jpeg", 
+".svgz", ".jpg", ".webp", ".ico", ".tif", ".pjpeg", ".avif"
+video/*
+".ogm", ".wmv", ".mpg", ".webm", ".ogv", ".mov", ".asx", ".mpeg", ".mp4", ".m4v", ".avi"
+audio/*
+".opus", ".flac", ".webm", ".weba", ".wav", ".ogg", ".m4a", ".oga", ".mid", ".mp3", ".aiff", ".wma", ".au"
+*/
 
 const filelist = {
 	required: createFileListValidatorWrapperFactory(
@@ -14,53 +46,54 @@ const filelist = {
 		() => ({ required: true })
 	),
 	files: {
-		type: {},
+		type: {
+			image: createFileListValidatorWrapperFactory(
+				value => assert(value, file => isImage(file.name)),
+				() => ({})
+			),
+			raster: createFileListValidatorWrapperFactory(
+				value => assert(value, file => isImage(file.name, "raster")),
+				() => ({})
+			),
+			vector: createFileListValidatorWrapperFactory(
+				value => assert(value, file => isImage(file.name, "vector")),
+				() => ({})
+			),
+			video: createFileListValidatorWrapperFactory(
+				value => assert(value, file => isVideo(file.name)),
+				() => ({})
+			),
+			audio: createFileListValidatorWrapperFactory(
+				value => assert(value, file => isAudio(file.name)),
+				() => ({})
+			),
+		},
 		size: {
 			gt(size: number, unit: ByteUnit = "b") {
 				const bytes = toBytes(size, unit)
 				return createFileListValidatorWrapperFactory(
-					value => {
-						for (let i = 0; i < value.length; i++) {
-							if (value[i].size <= bytes) return false
-						}
-						return true
-					},
+					value => assert(value, file => file.size > bytes),
 					() => ({})
 				)
 			},
 			gte(size: number, unit: ByteUnit = "b") {
 				const bytes = toBytes(size, unit)
 				return createFileListValidatorWrapperFactory(
-					value => {
-						for (let i = 0; i < value.length; i++) {
-							if (value[i].size < bytes) return false
-						}
-						return true
-					},
+					value => assert(value, file => file.size >= bytes),
 					() => ({})
 				)
 			},
 			lt(size: number, unit: ByteUnit = "b") {
 				const bytes = toBytes(size, unit)
 				return createFileListValidatorWrapperFactory(
-					value => {
-						for (let i = 0; i < value.length; i++) {
-							if (value[i].size >= bytes) return false
-						}
-						return true
-					},
+					value => assert(value, file => file.size < bytes),
 					() => ({})
 				)
 			},
 			lte(size: number, unit: ByteUnit = "b") {
 				const bytes = toBytes(size, unit)
 				return createFileListValidatorWrapperFactory(
-					value => {
-						for (let i = 0; i < value.length; i++) {
-							if (value[i].size > bytes) return false
-						}
-						return true
-					},
+					value => assert(value, file => file.size <= bytes),
 					() => ({})
 				)
 			},
@@ -68,13 +101,12 @@ const filelist = {
 				const bytesMin = toBytes(min, unit)
 				const bytesMax = toBytes(max, unit)
 				return createFileListValidatorWrapperFactory(
-					value => {
-						for (let i = 0; i < value.length; i++) {
-							if (value[i].size < bytesMin) return false
-							if (value[i].size > bytesMax) return false
-						}
-						return true
-					},
+					value =>
+						assert(
+							value,
+							file =>
+								file.size >= bytesMin && file.size <= bytesMax
+						),
 					() => ({})
 				)
 			},
@@ -82,41 +114,25 @@ const filelist = {
 				const bytesMin = toBytes(min, unit)
 				const bytesMax = toBytes(max, unit)
 				return createFileListValidatorWrapperFactory(
-					value => {
-						for (let i = 0; i < value.length; i++) {
-							if (
-								value[i].size >= bytesMin &&
-								value[i].size <= bytesMax
-							) {
-								return false
-							}
-						}
-						return true
-					},
+					value =>
+						assert(
+							value,
+							file => file.size < bytesMin || file.size > bytesMax
+						),
 					() => ({})
 				)
 			},
 			neq(size: number, unit: ByteUnit = "b") {
 				const bytes = toBytes(size, unit)
 				return createFileListValidatorWrapperFactory(
-					value => {
-						for (let i = 0; i < value.length; i++) {
-							if (value[i].size === bytes) return false
-						}
-						return true
-					},
+					value => assert(value, file => file.size !== bytes),
 					() => ({})
 				)
 			},
 			eq(size: number, unit: ByteUnit = "b") {
 				const bytes = toBytes(size, unit)
 				return createFileListValidatorWrapperFactory(
-					value => {
-						for (let i = 0; i < value.length; i++) {
-							if (value[i].size !== bytes) return false
-						}
-						return true
-					},
+					value => assert(value, file => file.size === bytes),
 					() => ({})
 				)
 			},
@@ -174,34 +190,11 @@ const filelist = {
 	},
 }
 
-/*
-
-{
-	validators: [filelist.required, filelist.length.eq(1)(""), filelist.files.size.gt()()]
+function assert(filelist: FileList, assertion: (file: File) => boolean) {
+	for (let i = 0; i < filelist.length; i++) {
+		if (!assertion(filelist[i])) return false
+	}
+	return true
 }
-
-let File = {
-	name: "black.png",
-	lastModified: 1643617999316,
-	webkitRelativePath: "",
-	size: 144, // bytes
-	type: "image/png",
-}
-{
-	name: "rotated-phone.xcf",
-	lastModified: 1644198965043,
-	webkitRelativePath: "",
-	size: 678609,
-	type: "image/x-xcf",
-}
-{
-	name: "rabbit.mp4",
-	lastModified: 1468153484000,
-	webkitRelativePath: "",
-	size: 346861418,
-	type: "video/mp4",
-}
-
-*/
 
 export default filelist
