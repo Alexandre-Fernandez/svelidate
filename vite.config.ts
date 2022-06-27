6293b10e900f6b12510c575b686f66252412bb9d
alexandre-fernandez <alexandre-fernandez@gmx.com>
2022-06-27 21:24:49 +0200

    alias

--- vite.config.ts
+++ vite.config.ts
@@ -1,10 +1,19 @@
 import { defineConfig } from "vite"
 import dts from "vite-plugin-dts"
+import { resolve } from "path"
+
+console.log(resolve(__dirname, "src/index.ts"))
 
 export default defineConfig({
+	resolve: {
+		alias: {
+			$src: resolve(__dirname, "src"),
+			$tests: resolve(__dirname, "tests"),
+		},
+	},
 	build: {
 		lib: {
-			entry: "src/index.ts",
+			entry: resolve(__dirname, "src/index.ts"),
 			name: "svelidate",
 			fileName: "svelidate",
 			formats: ["umd"],
