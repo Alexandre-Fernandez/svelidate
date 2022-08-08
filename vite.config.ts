1a6948e39fadfd46f0c0d33996791cc06322863e
alexandre-fernandez <alexandre-fernandez@gmx.com>
2022-08-08 11:15:37 +0200

    removed log

--- vite.config.ts
+++ vite.config.ts
@@ -2,8 +2,6 @@ import { defineConfig } from "vite"
 import dts from "vite-plugin-dts"
 import { resolve } from "path"
 
-console.log(resolve(__dirname, "src/index.ts"))
-
 export default defineConfig({
 	resolve: {
 		alias: {
